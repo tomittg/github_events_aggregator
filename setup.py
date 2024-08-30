bf64ea83848421bbd783e25c8142283cfd6c8fd5
Tomàs Treviño <ttrevinog@gmail.com>
2024-08-30 15:39:46 +0200

    Update setup.py adding a description and the parameters to include data

--- setup.py
+++ setup.py
@@ -10,4 +10,8 @@ setup(
         'pyyaml',
         'pytest'
     ],
+    description='Package that extracts github events from https://www.gharchive.org/ , performs some aggregation '
+                'operations over them and loads the result in csv and parquet formats',
+    include_package_data=True,
+    package_data={'github_events_aggregator': ['config/config.yaml']}
 )
