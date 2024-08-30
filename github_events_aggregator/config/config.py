675b1d6b3ef6994387d868d7a8c0dad1129427b0
Tomàs Treviño <ttrevinog@gmail.com>
2024-08-30 15:37:59 +0200

    Fix path error when executed from out of the project

--- github_events_aggregator/config/config.py
+++ github_events_aggregator/config/config.py
@@ -1,7 +1,9 @@
 import yaml
+from pathlib import Path
 
 
 def get_config():
-    with open('config/config.yaml', 'r') as config_file:
+    config_dir = Path(__file__).parent.resolve()
+    with open(f'{config_dir}/config.yaml', 'r') as config_file:
         config = yaml.safe_load(config_file)
     return config
